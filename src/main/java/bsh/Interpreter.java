27ac3c7735931422a40a0cf9236b312aa59ab9a0
revusky <revusky@gmail.com>
2023-01-09 11:54:15 +0100

    Quite minor changes. Have to write this.yield() instead of yield(). Decided to change some package names

--- src/main/java/bsh/Interpreter.java
+++ src/main/java/bsh/Interpreter.java
@@ -1013,7 +1013,7 @@ public class Interpreter
         try {
             return parser.Line();
         } catch (ParseException e) {
-            yield();
+            this.yield();
             if ( EOF )
                 return true;
             throw e;
